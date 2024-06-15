3bea0fe5146d88b1c0cddb560e2b64760c4c0d6a
starshininkirill <52084521+starshininkirill@users.noreply.github.com>
2024-06-15 20:59:24 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import asyncio
+from db import create_data
+import json
+
+API_TOKEN = '7325780980:AAHpIw6BQ1pDVhVBapU4MxNbUmms4VXFePA'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start_command(message: types.Message):
+    await message.answer("Напиши данные в формате ... и я тебе отвечу")
+
+
+@dp.message(lambda message: message.content_type ==  types.ContentType.TEXT)
+async def data_message(message: types.Message):
+
+    try:
+        data = json.loads(message.text)
+        dt_from = data['dt_from']
+        dt_upto = data['dt_upto']
+        group_type = data['group_type']
+        response = create_data(dt_from, dt_upto, group_type)
+        response = json.dumps(response)
+        await message.answer(response)
+    except:
+        await message.answer('Невалидный запос. Пример запроса:{"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}')
+
+
+async def main():
+    dp.message.register(start_command)
+    dp.message.register(data_message)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ db.py
@@ -0,0 +1,111 @@
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+
+client = MongoClient("localhost", 27017)
+db = client.testovoe
+collection = db.sample_collection
+
+
+def create_data(dt_from, dt_upto, group_by):
+    dt_from = datetime.fromisoformat(dt_from)
+    dt_upto = datetime.fromisoformat(dt_upto)
+
+    # Проверяем какая группировка выбрана
+    if group_by == 'month':
+        group_stage_fields = {
+            'year': {'$year': '$dt'},
+            'month': {'$month': '$dt'}
+        }
+        format_label = lambda x: f"{x['year']:04d}-{x['month']:02d}-01T00:00:00"
+
+    elif group_by == 'day':
+        group_stage_fields = {
+            'year': {'$year': '$dt'},
+            'month': {'$month': '$dt'},
+            'day': {'$dayOfMonth': '$dt'}
+        }
+        format_label = lambda x: f"{x['year']:04d}-{x['month']:02d}-{x['day']:02d}T00:00:00"
+
+    elif group_by == 'hour':
+        group_stage_fields = {
+            'year': {'$year': '$dt'},
+            'month': {'$month': '$dt'},
+            'day': {'$dayOfMonth': '$dt'},
+            'hour': {'$hour': '$dt'}
+        }
+        format_label = lambda x: f"{x['year']:04d}-{x['month']:02d}-{x['day']:02d}T{x['hour']:02d}:00:00"
+    else:
+        raise ValueError("no sort type matches")
+
+
+    # Фильтруем по дате
+    match_stage = {
+        '$match': {
+            'dt': {
+                '$gt': dt_from,
+                '$lt': dt_upto
+            }
+        }
+    }
+
+    # Группируем отфильтрованные значения
+    group_stage = {
+        '$group': {
+            '_id': group_stage_fields,
+            'total_value': {'$sum': '$value'}
+        }
+    }
+
+    # Сортируем сгруппированные значения
+    sort_stage = {
+        '$sort': {
+            '_id.year': 1,
+            '_id.month': 1,
+            '_id.day': 1,
+            '_id.hour': 1
+        }
+    }
+
+    pipeline = [match_stage, group_stage, sort_stage]
+    result = collection.aggregate(pipeline)
+
+    dataset = []
+    labels = []
+    result_dict = {}
+
+    # Форматируем результат агрегации в словарь для быстрого доступа
+    for doc in result:
+        label = format_label(doc['_id'])
+        result_dict[label] = doc['total_value']
+
+    current_date = dt_from
+    if group_by == 'month':
+        # Проходим циклом по всем датам от и до
+        while current_date <= dt_upto:
+            label = format_label({'year': current_date.year, 'month': current_date.month})
+            # Проверяем есть ли такая дата в результирующум массиве
+            if label not in result_dict:
+                # Если даты нет, то устанавливаем её значение в 0
+                result_dict[label] = 0
+            current_date += timedelta(days=32)
+            current_date = current_date.replace(day=1)
+    elif group_by == 'day':
+        while current_date <= dt_upto:
+            label = format_label({'year': current_date.year, 'month': current_date.month, 'day': current_date.day})
+            if label not in result_dict:
+                result_dict[label] = 0
+            current_date += timedelta(days=1)
+    elif group_by == 'hour':
+        while current_date <= dt_upto:
+            label = format_label({'year': current_date.year, 'month': current_date.month, 'day': current_date.day, 'hour': current_date.hour})
+            if label not in result_dict:
+                result_dict[label] = 0
+            current_date += timedelta(hours=1)
+
+    for label in sorted(result_dict):
+        labels.append(label)
+        dataset.append(result_dict[label])
+
+    return {"dataset": dataset, "labels": labels}
+
+
